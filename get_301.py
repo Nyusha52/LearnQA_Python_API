f8dad7d1361bfad84e623c8c63973766bb8d859a
Nadezhda Alekhnovich <n.alekhnovich@innopolis.ru>
2021-12-13 11:02:51 +0300

    long_redirect

--- get_301.py
+++ get_301.py
@@ -4,4 +4,6 @@ respons = requests.post('https://playground.learnqa.ru/api/long_redirect')
 first_response = respons.history
 for i in range(len(first_response)):
     print(first_response[i])
+    print(first_response[i].url)
 print(respons.status_code)
+print(respons.url)
