07834da2b1d2b5780f70634306f245d99dd74261
Nadezhda Alekhnovich <n.alekhnovich@innopolis.ru>
2021-12-21 11:55:35 +0300

    homework_cookie

--- test_homework_cookie.py
+++ test_homework_cookie.py
@@ -2,6 +2,5 @@ import requests
 class TestCookie:
     def test_cookie(self):
         respons = requests.get('https://playground.learnqa.ru/api/homework_cookie')
-        cookies_respons = dict(respons.cookies)
-        print(cookies_respons)
+        cookies_respons = {'HomeWork': 'hw_value'}
         assert cookies_respons == dict(respons.cookies)
