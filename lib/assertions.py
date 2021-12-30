2e53557235dcd5313297144be55edf80ae458f89
Nadezhda Alekhnovich <n.alekhnovich@innopolis.ru>
2021-12-30 12:20:10 +0300

    Ex15: Тесты на метод user

--- lib/assertions.py
+++ lib/assertions.py
@@ -11,3 +11,36 @@ class Assertions:
 
         assert name in response_as_dict, f"ошибка {name}"
         assert response_as_dict[name] == expected_value, error_message
+
+    @staticmethod
+    def assert_json_has_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"ошибка {response.text}"
+
+        assert name in response_as_dict, f"ошибка {name}"
+
+    @staticmethod
+    def assert_json_has_no_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"ошибка {response.text}"
+
+        assert name not in response_as_dict, f"ошибка {name}"
+
+    @staticmethod
+    def assert_json_has_keys(response: Response, names: list):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"ошибка {response.text}"
+
+        for name in names:
+            assert name in response_as_dict, f"ошибка {name}"
+
+    @staticmethod
+    def assert_code_status(response: Response, expected_status_code):
+        assert response.status_code == expected_status_code, f"ошибка {response.status_code}"
+
